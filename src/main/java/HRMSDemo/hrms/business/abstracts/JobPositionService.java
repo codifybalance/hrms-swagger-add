00a24732443ccb4ae038e918f685f5425d589505
Ayça Aydemir <40012179+codifybalance@users.noreply.github.com>
2021-06-09 22:08:41 +0300

    hrms-swagger-add
    hrms-7 gün 1 ödev

---
+++ src/main/java/HRMSDemo/hrms/business/abstracts/JobPositionService.java
@@ -0,0 +1,12 @@
+package HRMSDemo.hrms.business.abstracts;
+
+import java.util.List;
+
+import HRMSDemo.hrms.core.utilities.results.DataResult;
+import HRMSDemo.hrms.core.utilities.results.Result;
+import HRMSDemo.hrms.entities.concretes.JobPosition;
+
+public interface JobPositionService {
+	DataResult<List<JobPosition>> getAll();
+	Result add(JobPosition jobPosition);
+}
---
+++ src/main/java/HRMSDemo/hrms/business/concretes/JobPositionManager.java
@@ -0,0 +1,46 @@
+package HRMSDemo.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import HRMSDemo.hrms.business.abstracts.JobPositionService;
+import HRMSDemo.hrms.core.utilities.results.DataResult;
+import HRMSDemo.hrms.core.utilities.results.Result;
+import HRMSDemo.hrms.core.utilities.results.SuccessDataResult;
+import HRMSDemo.hrms.core.utilities.results.SuccessResult;
+import HRMSDemo.hrms.dataAccess.abstracts.JobPositionDao;
+import HRMSDemo.hrms.entities.concretes.JobPosition;
+@Service
+public class JobPositionManager implements JobPositionService{
+	private JobPositionDao jobPositionDao; 
+	
+	@Autowired
+	public JobPositionManager (JobPositionDao jobPositionDao) {
+		super();
+		this.jobPositionDao =jobPositionDao;
+	}
+	
+	
+	/*@Override
+	public List<JobPosition> getAll() {
+		// TODO Auto-generated method stub
+		return this.jobPositionDao.findAll();
+	}*/
+	
+	@Override
+	public DataResult<List<JobPosition>> getAll() {
+
+		return new SuccessDataResult<List<JobPosition>>
+		(this.jobPositionDao.findAll(),"Data listelendi");
+		}
+
+
+	@Override
+	public Result add(JobPosition jobPosition) {
+		this.jobPositionDao.save(jobPosition);
+		return new SuccessResult("Ürün eklendi");
+	}
+
+}
